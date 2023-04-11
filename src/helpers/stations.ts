f023e6cba21e622bcbabe657d82c8bfa706a8959
Ivan Kotov <iamivankotov@gmail.com>
2023-04-11 19:32:16 +0200

    hc stations

---
+++ src/helpers/stations.ts
@@ -0,0 +1,24 @@
+import {StationOptions} from '../types';
+
+export const stations: StationOptions[] = [
+  {
+    name: 'poble sec',
+    prevTime: 0,
+    id: '1',
+  },
+  {
+    name: 'paralel',
+    prevTime: 120,
+    id: '2',
+  },
+  {
+    name: 'drassanes',
+    prevTime: 160,
+    id: '3',
+  },
+  {
+    name: 'liceu',
+    prevTime: 180,
+    id: '4',
+  },
+];
